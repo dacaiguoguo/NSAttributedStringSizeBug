de874502a26796b2bbbc71879c36112132c8d91d
dacaiguoguo <dacaiguoguo@gmail.com>
2020-07-09 15:34:03 +0800

    More Test

--- NSAttributedStringSizeBug/ViewController.m
+++ NSAttributedStringSizeBug/ViewController.m
@@ -8,8 +8,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UILabel *testOneLineLabel;
 @property (strong, nonatomic) IBOutlet UILabel *testLabel;
 @property (strong, nonatomic) IBOutlet NSLayoutConstraint *testLabelHeightLayout;
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *testOneLineHeightLayout;
 
 @end
 
@@ -27,15 +29,29 @@
     NSDictionary *att = @{NSParagraphStyleAttributeName: paraStyle,
                           NSFontAttributeName: font,
                           NSForegroundColorAttributeName: [UIColor darkTextColor]};
-     NSString *testContent = @"首行缩进 测试首行缩进 测试测测试首行缩进 测试测首行缩进 测试首行缩进 测试测测试首行缩进 测试测";
-//    NSString *testContent = @"首行缩进 测试首行缩进 测试";
+
+    NSString *testOneLineContent = @"首行缩进 测试首行缩进 测试";
+
+    NSAttributedString *attrOneLineText = [[NSAttributedString alloc] initWithString:testOneLineContent attributes:att];
+    CGRect rectOneLine = [attrOneLineText boundingRectWithSize:CGSizeMake(labelWitdh, 1000) options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading context:nil];
+    CGFloat nameOneLineHeight = ceilf(CGRectGetHeight(rectOneLine));
+    self.testOneLineLabel.attributedText = attrOneLineText;
+    self.testOneLineLabel.layer.borderWidth = .5;
+    NSLog(@"nameOneLineHeight:%lf", nameOneLineHeight);
+    self.testOneLineHeightLayout.constant = nameOneLineHeight;
+
+
+
+    NSString *testContent = @"首行缩进 测试首行缩进 测试测测试首行缩进 测试测首行缩进 测试首行缩进 测试测测试首行缩进 测试测";
+
     NSAttributedString *attrText = [[NSAttributedString alloc] initWithString:testContent attributes:att];
     CGRect rect = [attrText boundingRectWithSize:CGSizeMake(labelWitdh, 1000) options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading context:nil];
     CGFloat nameHeight = ceilf(CGRectGetHeight(rect));
     self.testLabel.attributedText = attrText;
     self.testLabel.layer.borderWidth = .5;
-    NSLog(@"%lf", nameHeight);
+    NSLog(@"nameHeight:%lf", nameHeight);
     self.testLabelHeightLayout.constant = nameHeight;
+
 
     // NSAttributedString 高度计算的陷阱：当原来的文本只有一行的时候，不会因为首行缩进变成两行，而且超出边界文本被截掉
     // 只有首行缩进为0时是多行的，增加首行缩进才对高度计算有影响
